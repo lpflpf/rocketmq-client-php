7b95054d2a9a0b40ec7fbf3fccbc31550430c3c9
lipengfei <634626165@qq.com>
2019-01-24 14:19:49 +0800

    autocomplete add sendResult & sendStatus

---
+++ autocomplete/SendResult.php
@@ -0,0 +1,14 @@
+<?php
+
+class SendResult{
+    public function getMsgId(){
+    }
+    public function getOffsetMsgId(){
+    }
+    public function getSendStatus(){
+    }
+    public function getMessageQueue(){
+    }
+    public function getQueueOffset(){
+    }
+}
---
+++ autocomplete/SendStatus.php
@@ -0,0 +1,8 @@
+<?php
+
+class SendStatus{
+    const SEND_OK;
+    const SEND_FLUSH_DISK_TIMEOUT;
+    const SEND_FLUSH_SLAVE_TIMEOUT;
+    const SEND_SLAVE_NOT_AVAILABLE;
+}
