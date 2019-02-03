6f2aff1d4232b934549d0ec387a3fecad04af4fc
lipengfei <634626165@qq.com>
2019-02-03 14:02:48 +0800

    bugfix autocompelete

--- autocomplete/PullConsumer.php
+++ autocomplete/PullConsumer.php
@@ -34,10 +34,10 @@ class PullConsumer{
     public function setGroup(string $group){
     }
 
-    public function pull(Rocketmq\\MessageQueue $messageQueue, string $subExpression, int $offset, int $maxNums){
+    public function pull(MessageQueue $messageQueue, string $subExpression, int $offset, int $maxNums){
     }
 
-    public function pullBlockIfNotFound(Rocketmq\\MessageQueue $messageQueue, string $subExpression, int $offset, int $maxNums){
+    public function pullBlockIfNotFound(MessageQueue $messageQueue, string $subExpression, int $offset, int $maxNums){
     }
 
     public function getSessionCredentials(){
@@ -46,13 +46,13 @@ class PullConsumer{
     public function setSessionCredentials(string $accessKey, string $secretKey, string $authChannel){
     }
 
-    void updateConsumeOffset(Rocketmq\\MessageQueue $messageQueue, int $offset){
+    public function updateConsumeOffset(MessageQueue $messageQueue, int $offset){
     }
 
-    public function removeConsumeOffset(Rocketmq\\MessageQueue $messageQueue){
+    public function removeConsumeOffset(MessageQueue $messageQueue){
     }
 
-    public function fetchConsumeOffset(Rocketmq\\MessageQueue $messageQueue, string $fromStore){
+    public function fetchConsumeOffset(MessageQueue $messageQueue, string $fromStore){
     }
 
     public function setMessageModel($model){
--- autocomplete/SendStatus.php
+++ autocomplete/SendStatus.php
@@ -3,8 +3,8 @@
 namespace RocketMQ;
 
 class SendStatus{
-    const SEND_OK;
-    const SEND_FLUSH_DISK_TIMEOUT;
-    const SEND_FLUSH_SLAVE_TIMEOUT;
-    const SEND_SLAVE_NOT_AVAILABLE;
+    const SEND_OK = 0;
+    const SEND_FLUSH_DISK_TIMEOUT = 1;
+    const SEND_FLUSH_SLAVE_TIMEOUT = 2;
+    const SEND_SLAVE_NOT_AVAILABLE = 3;
 }
