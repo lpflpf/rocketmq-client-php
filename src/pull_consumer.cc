5799b3416556df7da505f2deebfb2809776832d9
lipengfei <634626165@qq.com>
2019-02-26 11:32:00 +0800

    ADD LICENSE

--- src/pull_consumer.cc
+++ src/pull_consumer.cc
@@ -1,3 +1,20 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 #include "pull_consumer.h"
 #include "session_credentials.h"
 
@@ -19,7 +36,6 @@ Php::Value PullConsumer::getQueues(){
     Php::Array result;
     int idx = 0;
     this->consumer->fetchSubscribeMessageQueues(this->topicName, this->mqs);
-    std::cout << this->mqs.size() << std::endl;
     std::vector<rocketmq::MQMessageQueue>::iterator iter = mqs.begin();
     for (; iter != mqs.end(); ++iter) {
         rocketmq::MQMessageQueue mq = (*iter);
